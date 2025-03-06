b7678cd1598969360ae365e77c6582813119387a
OliverLudford <oliverlukeludford@gmail.com>
2025-03-06 14:45:28 +0000

    Added constructors for Player and Room along with basic functionality

--- Game.cs
+++ Game.cs
@@ -10,16 +10,45 @@ namespace DungeonExplorer
 
         public Game()
         {
-            // Initialize the game with one room and one player
+            Console.WriteLine("Welcome to Dungeon Explorer");
+            Console.Write("Please enter your character's name: ");
+            string playerName = Console.ReadLine(); // Get user input for the players name
 
+            player = new Player(playerName, 100);
+            currentRoom = new Room("You are stood in a dark room that contains a small chest"); // Constructs player and room
         }
+
         public void Start()
         {
-            // Change the playing logic into true and populate the while loop
-            bool playing = false;
+            Console.WriteLine($"Welcome {player.Name}");
+            bool playing = true;
             while (playing)
             {
-                // Code your playing logic here
+                Console.WriteLine("What would you like to do? (input 1-3)"); // gets player input
+                Console.WriteLine("1 = Look at the room");
+                Console.WriteLine("2 = Check inventory");
+                Console.WriteLine("3 = Quit game");
+                string playerAction = Console.ReadLine();
+
+                switch (playerAction)
+                {
+                    case "1":
+                        Console.WriteLine(currentRoom.GetDescription());
+                        break;
+
+                    case "2":
+                        Console.WriteLine($"Inventory: {player.InventoryContents()}");
+                        break;
+
+                    case "3":
+                        Console.WriteLine($"Thank you for playing {player.Name}");
+                        playing = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Please enter a valid input (1, 2 or 3)");
+                        break;
+                }
             }
         }
     }
