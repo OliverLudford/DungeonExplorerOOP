b01ab7b7a980deaa838c5baa7e3057fe35a7af82
OliverLudford <oliverlukeludford@gmail.com>
2025-03-07 13:41:01 +0000

    Improved readability of the program

--- Game.cs
+++ Game.cs
@@ -26,13 +26,16 @@ namespace DungeonExplorer
             bool playing = true;
             while (playing)
             {
-                Console.WriteLine("\nWhat would you like to do? (input 1-3)"); // gets player input
+                Console.WriteLine("----------------------------------------------");
+                Console.WriteLine("\nWhat would you like to do next? (input 1-3)"); // gets player input
                 Console.WriteLine("\n1 = Look at the room");
                 Console.WriteLine("2 = Check Health and Inventory");
                 Console.WriteLine("3 = Pickup Item");
-                Console.WriteLine("4 = Quit game");
+                Console.WriteLine("4 = Move to the next area");
+                Console.WriteLine("5 = Quit game");
 
                 string playerAction = Console.ReadLine();
+                Console.Clear(); // Clears existing text for better readablitiy
 
                 switch (playerAction)
                 {
@@ -41,21 +44,26 @@ namespace DungeonExplorer
                         break;
 
                     case "2":
-                        Console.WriteLine($"\nYour Health is currently: {player.Health}");
-                        Console.WriteLine($"Inventory: {player.InventoryContents()}");
+                        Console.WriteLine($"Your Health is currently: {player.Health}");
+                        Console.WriteLine($"\nInventory:\n{player.InventoryContents()}");
                         break;
-
-                    case "4":
-                        Console.WriteLine($"\nThank you for playing {player.Name}");
-                        playing = false;
-                        break;
-
+                
                     case "3":
                         player.PickUpItem(currentRoom);
                         break;
 
+                    case "4":
+                        currentRoom = Room.GetRandomRoom(); // Constructs another random room, possibly with an item
+                        Console.WriteLine(currentRoom.GetDescription(currentRoom)); // Prints the description for the new room
+                        break;
+
+                    case "5":
+                        Console.WriteLine($"\nThank you for playing {player.Name}"); // quits the game
+                        playing = false;
+                        break;
+
                     default:
-                        Console.WriteLine("\nPlease enter a valid input (1, 2 or 3)");
+                        Console.WriteLine("\nPlease enter a valid input (1, 2 or 3)"); // Handles invalid inputs
                         break;
                 }
             }
